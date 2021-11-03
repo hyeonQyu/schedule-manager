bbb243f11420b55fe11cab08e329a3336e6a9821
Hyeongyu Kim <dhk0561@naver.com>
2021-11-03 22:56:08 +0900

    feat: 구글로 허용된 계정만 로그인

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,12 +2,29 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import classNames from 'classnames/bind';
 import style from './Login.scss';
-import { RouteProps } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
+import GoogleIcon from '@icons/google/GoogleIcon';
+import UserStore from '@stores/UserStore';
 
 const cx = classNames.bind(style);
 
-const Login = observer((props: RouteProps) => {
-    return <div>로그인 페이지</div>;
+const userStore = UserStore.instance;
+
+const Login = observer((props: RouteComponentProps) => {
+    const { continueWithGoogle, isLoggedIn } = userStore;
+
+    if (isLoggedIn) props.history.push('/');
+
+    return (
+        <div className={cx('wrapper')}>
+            <button onClick={continueWithGoogle}>
+                <div className={cx('icon_google')}>
+                    <GoogleIcon />
+                </div>
+                <span>구글 계정으로 계속하기</span>
+            </button>
+        </div>
+    );
 });
 
 export default Login;
--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,14 +1,27 @@
 import { autobind } from 'core-decorators';
-import { observable } from 'mobx';
+import { action, observable } from 'mobx';
+import { authService } from 'firebaseService.js';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { dialog } from '@components/common/dialog/Dialog';
+import env from 'env.js';
 
 @autobind
 export default class UserStore {
     private static _instance: UserStore;
 
     @observable private _isLoggedIn: boolean = false;
+    @observable private _userEmail: string = '';
 
     private constructor() {
         UserStore._instance = this;
+
+        authService.onAuthStateChanged((user) => {
+            if (user && this.isRightUser(user)) {
+                this._isLoggedIn = true;
+                return;
+            }
+            this._isLoggedIn = false;
+        });
     }
 
     static get instance() {
@@ -20,5 +33,38 @@ export default class UserStore {
 
     get isLoggedIn(): boolean {
         return this._isLoggedIn;
+    }
+
+    /**
+     * 구글 계정으로 로그인
+     */
+    @action
+    continueWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        (async () => {
+            try {
+                const result = await signInWithPopup(authService, provider);
+                if (this.isRightUser(result.user)) {
+                    this._isLoggedIn = true;
+                }
+            } catch (e) {}
+        })();
+    }
+
+    /**
+     * 허용된 사용자인지 검사
+     * @param user
+     * @private
+     */
+    private isRightUser(user) {
+        const { email } = user;
+        for (let i = 0; i < env.MAIL_ACCOUNTS.length; i++) {
+            if (email === env.MAIL_ACCOUNTS[i]) {
+                this._userEmail = email;
+                return true;
+            }
+        }
+        dialog.alert('허용된 계정이 아닙니다.');
+        return false;
     }
 }

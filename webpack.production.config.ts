4d0a493ddc862a367a78347055dc1d43ec0dc776
Hyeongyu Kim <dhk0561@naver.com>
2021-11-26 22:43:09 +0900

    build: 웹팩 임시 배포용 설정 추가

---
+++ webpack.production.config.ts
@@ -0,0 +1,85 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+
+const config = {
+    mode: 'production',
+    output: {
+        publicPath: '/',
+    },
+    entry: './src/index.tsx',
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    }, // creates style nodes from JS strings
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]--[hash:base64:5]',
+                            },
+                            sourceMap: true,
+                        },
+                    }, // translates CSS into CommonJS
+                    {
+                        loader: 'resolve-url-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    }, // compiles Sass to CSS, using Node Sass by default
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.txt$/,
+                use: 'raw-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [
+            new TsconfigPathsPlugin({
+                configFile: path.resolve(__dirname, './tsconfig.json'),
+            }),
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 4000,
+    },
+};
+
+export default config;

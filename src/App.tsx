04fc7fefa65d6d5db91ca828f77b92389d341480
Hyeongyu Kim <dhk0561@naver.com>
2021-12-02 00:09:15 +0900

    feat: ScheduleAddModal App에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.scss';
 import AppRouter from '@components/app-router/AppRouter';
+import ScheduleAddModal from '@components/schedule-modal/components/add/ScheduleAddModal';
 
 const App = () => {
-    return <AppRouter />;
+    return (
+        <>
+            <AppRouter />
+            <ScheduleAddModal />
+        </>
+    );
 };
 
 export default App;

17203f8f43c61c8bfd10a7627c2e3cf5075e96be
100Gyeon <bjy9966@naver.com>
2021-12-04 00:48:49 +0900

    chore: router v6 업그레이드

--- src/components/app-router/AppRouter.tsx
+++ src/components/app-router/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import UserStore from '@stores/UserStore';
 import Login from '@pages/login/Login';
 import Home from '@pages/home/Home';
@@ -13,11 +13,11 @@ const userStore = UserStore.instance;
 const AppRouter = observer(() => {
     const { isLoggedIn } = userStore;
     return (
-        <Switch>
-            {isLoggedIn ? <Route exact path={'/'} component={Home} /> : <Route exact path={'/'} component={Login} />}
-            <ProtectedRoute exact path={'/my'} component={MySchedule} />
-            <ProtectedRoute exact path={'/other'} component={OtherSchedule} />
-        </Switch>
+        <Routes>
+            {isLoggedIn ? <Route path={'/'} element={<Home />} /> : <Route path={'/'} element={<Login />} />}
+            <ProtectedRoute path={'/my'} element={<MySchedule />} />
+            <ProtectedRoute path={'/other'} element={<OtherSchedule />} />
+        </Routes>
     );
 });
 

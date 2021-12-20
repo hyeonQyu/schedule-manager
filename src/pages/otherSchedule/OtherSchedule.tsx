1fb7f40305129a10fc0bda5304704d6bd3c88ad5
100Gyeon <bjy9966@naver.com>
2021-12-20 16:51:48 +0900

    chore: RouteComponentProps 관련 에러 제거

--- src/pages/otherSchedule/OtherSchedule.tsx
+++ src/pages/otherSchedule/OtherSchedule.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { observer } from 'mobx-react';
 import Layout from '@components/layout/Layout';
 
-const OtherSchedule = (props: RouteComponentProps) => {
-    return <Layout {...props} />;
-};
+const OtherSchedule = observer(() => {
+    return <Layout></Layout>;
+});
 
 export default OtherSchedule;

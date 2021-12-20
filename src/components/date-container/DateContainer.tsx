86bd12c4f02f5a2e2c3b32ca57421e86e5a6bfff
100Gyeon <bjy9966@naver.com>
2021-12-20 23:42:26 +0900

    chore: DateContainer 컴포넌트 분리

---
+++ src/components/date-container/DateContainer.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import classNames from 'classnames/bind';
+import style from './DateContainer.scss';
+import Card from '@components/card/Card';
+import Datetime from '@utils/Datetime';
+
+const cx = classNames.bind(style);
+
+const DateContainer = observer(() => {
+    const showSchedule = () => {
+        const myCardArr = [],
+            otherCardArr = [];
+        for (let i = 0; i < 2; i++) {
+            myCardArr.push(
+                <Card
+                    className={cx('my-card')}
+                    key={`my-card-${i}`}
+                    schedule={{
+                        startDatetime: new Datetime(2021, 11, 12, 10, 0),
+                        endDatetime: new Datetime(2021, 11, 12, 10, 50),
+                        name: 'schedule',
+                    }}
+                />,
+            );
+            otherCardArr.push(
+                <Card
+                    className={cx('other-card')}
+                    key={`other-card-${i}`}
+                    schedule={{
+                        startDatetime: new Datetime(2021, 11, 12, 10, 0),
+                        endDatetime: new Datetime(2021, 11, 12, 10, 50),
+                        name: 'schedule',
+                    }}
+                />,
+            );
+        }
+        const resultArr = [...myCardArr, ...otherCardArr];
+        return resultArr;
+    };
+    return (
+        <div className={cx('wrapper')}>
+            <h2>✔ 2021.11.12</h2>
+            <div className={cx('schedule-container')}>
+                <div className={cx('schedule-line')}></div>
+                <div className={cx('schedule-info')}>{showSchedule()}</div>
+            </div>
+        </div>
+    );
+});
+
+export default DateContainer;
--- src/pages/mySchedule/MySchedule.tsx
+++ src/pages/mySchedule/MySchedule.tsx
@@ -2,50 +2,20 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import Layout from '@components/layout/Layout';
 import DateSlider from '@components/common/date-slider/DateSlider';
-import Card from '@components/card/Card';
-import Datetime from '@utils/Datetime';
 import classNames from 'classnames/bind';
 import style from './MySchedule.scss';
+import DateContainer from '@components/date-container/DateContainer';
 
 const cx = classNames.bind(style);
 
 const MySchedule = observer(() => {
-    const showSchedule = () => {
-        const myCardArr = [],
-            otherCardArr = [];
-        for (let i = 0; i < 3; i++) {
-            myCardArr.push(
-                <Card
-                    className={cx('my-card')}
-                    key={`my-card-${i}`}
-                    schedule={{
-                        startDatetime: new Datetime(2021, 11, 12, 10, 0),
-                        endDatetime: new Datetime(2021, 11, 12, 10, 50),
-                        name: 'schedule',
-                    }}
-                />,
-            );
-            otherCardArr.push(
-                <Card
-                    className={cx('other-card')}
-                    key={`other-card-${i}`}
-                    schedule={{
-                        startDatetime: new Datetime(2021, 11, 12, 10, 0),
-                        endDatetime: new Datetime(2021, 11, 12, 10, 50),
-                        name: 'schedule',
-                    }}
-                />,
-            );
-        }
-        const resultArr = [...myCardArr, ...otherCardArr];
-        return resultArr;
-    };
     return (
         <Layout>
             <div className={cx('wrapper')}>
                 <h1>Weekly Schedule</h1>
                 <DateSlider />
-                {showSchedule()}
+                <DateContainer />
+                <DateContainer />
             </div>
         </Layout>
     );

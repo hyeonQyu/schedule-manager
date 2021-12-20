0ab741000b94e01a01ffba2c88289b1382983a8e
100Gyeon <bjy9966@naver.com>
2021-12-20 21:40:54 +0900

    feat: MySchedule 페이지 구조화 및 스타일링

--- src/pages/mySchedule/MySchedule.tsx
+++ src/pages/mySchedule/MySchedule.tsx
@@ -1,10 +1,54 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import Layout from '@components/layout/Layout';
-import { RouteComponentProps } from 'react-router-dom';
+import DateSlider from '@components/common/date-slider/DateSlider';
+import Card from '@components/card/Card';
+import Datetime from '@utils/Datetime';
+import classNames from 'classnames/bind';
+import style from './MySchedule.scss';
 
-const MySchedule = observer((props: RouteComponentProps) => {
-    return <Layout {...props} />;
+const cx = classNames.bind(style);
+
+const MySchedule = observer(() => {
+    const showSchedule = () => {
+        const myCardArr = [],
+            otherCardArr = [];
+        for (let i = 0; i < 3; i++) {
+            myCardArr.push(
+                <Card
+                    className={cx('my-card')}
+                    key={`my-card-${i}`}
+                    schedule={{
+                        startDatetime: new Datetime(2021, 11, 12, 10, 0),
+                        endDatetime: new Datetime(2021, 11, 12, 10, 50),
+                        name: 'schedule',
+                    }}
+                />,
+            );
+            otherCardArr.push(
+                <Card
+                    className={cx('other-card')}
+                    key={`other-card-${i}`}
+                    schedule={{
+                        startDatetime: new Datetime(2021, 11, 12, 10, 0),
+                        endDatetime: new Datetime(2021, 11, 12, 10, 50),
+                        name: 'schedule',
+                    }}
+                />,
+            );
+        }
+        const resultArr = [...myCardArr, ...otherCardArr];
+        return resultArr;
+    };
+    return (
+        <Layout>
+            <div className={cx('wrapper')}>
+                <h1>Weekly Schedule</h1>
+                <DateSlider />
+                {showSchedule()}
+            </div>
+        </Layout>
+    );
 });
 
 export default MySchedule;

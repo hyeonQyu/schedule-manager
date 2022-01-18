a7400d01bfd9fca509b71bb9657436a96e62fdc0
Hyeongyu Kim <dhk0561@naver.com>
2022-01-18 22:19:57 +0900

    build: build mode 별 환경 변수 설정

--- webpack.dev.config.ts
+++ webpack.dev.config.ts
@@ -2,84 +2,93 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import dotenv from 'dotenv';
+import loadEnv from './env/loadEnv.js';
 
-const config = {
-    mode: 'development',
-    output: {
-        publicPath: '/',
-    },
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+export default (env) => {
+    dotenv.config({
+        path: path.resolve(__dirname, `./env/${env.mode}.env`),
+    });
+
+    return {
+        mode: 'development',
+        output: {
+            publicPath: '/',
+        },
+        entry: './src/index.tsx',
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/i,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    }, // creates style nodes from JS strings
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]--[hash:base64:5]',
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                        }, // creates style nodes from JS strings
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    mode: 'local',
+                                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                                },
+                                sourceMap: true,
                             },
-                            sourceMap: true,
+                        }, // translates CSS into CommonJS
+                        {
+                            loader: 'resolve-url-loader',
                         },
-                    }, // translates CSS into CommonJS
-                    {
-                        loader: 'resolve-url-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    }, // compiles Sass to CSS, using Node Sass by default
-                ],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: 'file-loader',
-            },
-            {
-                test: /\.txt$/,
-                use: 'raw-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        }, // compiles Sass to CSS, using Node Sass by default
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    use: 'file-loader',
+                },
+                {
+                    test: /\.txt$/,
+                    use: 'raw-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    configFile: path.resolve(__dirname, './tsconfig.json'),
+                }),
+            ],
+        },
         plugins: [
-            new TsconfigPathsPlugin({
-                configFile: path.resolve(__dirname, './tsconfig.json'),
+            new HtmlWebpackPlugin({
+                template: 'public/index.html',
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
+            new webpack.DefinePlugin({
+                'process.env': loadEnv(process.env),
             }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        }),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 4000,
-    },
+        devtool: 'inline-source-map',
+        devServer: {
+            port: 4000,
+        },
+    };
 };
-
-export default config;
--- webpack.production.config.ts
+++ webpack.production.config.ts
@@ -3,92 +3,98 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import dotenv from 'dotenv';
+import loadEnv from './env/loadEnv';
 
-const config = {
-    mode: 'production',
-    output: {
-        publicPath: './',
-        filename: 'bundle.[hash].js',
-        sourceMapFilename: 'bundle.[hash].js.map',
-    },
-    entry: './src/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+export default (env) => {
+    dotenv.config({
+        path: path.resolve(__dirname, `./env/${env.mode}.env`),
+    });
+
+    return {
+        mode: 'production',
+        output: {
+            publicPath: './',
+            filename: 'bundle.[hash].js',
+            sourceMapFilename: 'bundle.[hash].js.map',
+        },
+        entry: './src/index.tsx',
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/i,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                        },
                     },
                 },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    }, // creates style nodes from JS strings
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]--[hash:base64:5]',
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                        }, // creates style nodes from JS strings
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    mode: 'local',
+                                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                                },
+                                sourceMap: true,
                             },
-                            sourceMap: true,
+                        }, // translates CSS into CommonJS
+                        {
+                            loader: 'resolve-url-loader',
                         },
-                    }, // translates CSS into CommonJS
-                    {
-                        loader: 'resolve-url-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
-                        },
-                    }, // compiles Sass to CSS, using Node Sass by default
-                ],
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: 'file-loader',
-            },
-            {
-                test: /\.txt$/,
-                use: 'raw-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        }, // compiles Sass to CSS, using Node Sass by default
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    use: 'file-loader',
+                },
+                {
+                    test: /\.txt$/,
+                    use: 'raw-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    configFile: path.resolve(__dirname, './tsconfig.json'),
+                }),
+            ],
+        },
         plugins: [
-            new TsconfigPathsPlugin({
-                configFile: path.resolve(__dirname, './tsconfig.json'),
+            new HtmlWebpackPlugin({
+                template: 'public/index.html',
+                minify: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                },
+            }),
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
+            new webpack.DefinePlugin({
+                'process.env': loadEnv(process.env),
+            }),
+            new CleanWebpackPlugin({
+                cleanAfterEveryBuildPatterns: [path.resolve(__dirname, './dist')],
             }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-            minify: {
-                collapseWhitespace: true,
-                removeComments: true,
-            },
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        }),
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ['dist'],
-        }),
-    ],
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 4000,
-    },
+        devtool: 'inline-source-map',
+    };
 };
-
-export default config;

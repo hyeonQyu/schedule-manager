59aa3c6202e3102d7b63094d0718504802164884
100Gyeon <bjy9966@naver.com>
2022-02-03 04:56:51 +0900

    chore: my-card, other-card 세팅하는 부분 수정

--- src/components/date-container/DateContainer.tsx
+++ src/components/date-container/DateContainer.tsx
@@ -5,6 +5,7 @@ import style from './DateContainer.scss';
 import Card from '@components/card/Card';
 import WeeklyScheduleStore from '@stores/WeeklyScheduleStore';
 import UserStore from '@stores/UserStore';
+import { dayArray } from '@defines/defines';
 
 const cx = classNames.bind(style);
 
@@ -14,54 +15,34 @@ const userStore = UserStore.instance;
 const DateContainer = observer(() => {
     const { thisWeekArray, thisWeekSchedule } = weekStore;
 
-    const sortSchedule = (a, b) => {
-        const dateA = new Date(a.scheduleDate).getTime();
-        const dateB = new Date(b.scheduleDate).getTime();
-        return dateA > dateB ? -1 : 1;
-    }
-
     const showSchedule = () => {
         const cardArray = [];
         {
             thisWeekSchedule &&
                 thisWeekSchedule.map((schedule) =>
-                    schedule.owner === userStore.userEmail
-                        ? cardArray.push(
-                            <Card
-                                className={cx('my-card')}
-                                key={schedule.name}
-                                schedule={{
-                                    owner: schedule.owner,
-                                    scheduleDate: schedule.scheduleDate,
-                                    startTime: schedule.startTime,
-                                    endTime: schedule.endTime,
-                                    name: schedule.name,
-                                }}
-                            />,
-                        )
-                        : cardArray.push(
-                            <Card
-                                className={cx('other-card')}
-                                key={schedule.name}
-                                schedule={{
-                                    owner: schedule.owner,
-                                    scheduleDate: schedule.scheduleDate,
-                                    startTime: schedule.startTime,
-                                    endTime: schedule.endTime,
-                                    name: schedule.name,
-                                }}
-                            />,
-                        ),
+                    cardArray.push(
+                        <Card
+                            className={schedule.owner === userStore.userEmail ? cx('my-card') : cx('other-card')}
+                            key={schedule.name}
+                            schedule={{
+                                owner: schedule.owner,
+                                scheduleDate: schedule.scheduleDate,
+                                startTime: schedule.startTime,
+                                endTime: schedule.endTime,
+                                name: schedule.name,
+                            }}
+                        />,
+                    ),
                 );
         }
-        return cardArray.sort(sortSchedule);
+        return cardArray;
     };
 
     return (
         <div className={cx('wrapper')}>
-            {thisWeekArray.map((thisWeek) => (
-                <div key={`${thisWeek.year}.${thisWeek.month}.${thisWeek.date}`}>
-                    <h2>✔ {`${thisWeek.year}.${thisWeek.month}.${thisWeek.date}`}</h2>
+            {thisWeekArray.map(({ year, month, date }) => (
+                <div key={`${year}.${month}.${date}`}>
+                    <h2>✔ {`${year}.${month}.${date} (${dayArray[new Date(year, month - 1, date).getDay()]})`}</h2>
                     <div className={cx('schedule-container')}>
                         <div className={cx('schedule-line')} />
                         <div className={cx('schedule-info')}>{showSchedule()}</div>
